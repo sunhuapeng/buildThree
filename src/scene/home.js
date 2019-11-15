029466683b202ea8485cf485d587b0d6aec58750
sunhuapeng <1186781410@qq.com>
2019-11-15 12:52:21 +0800

    初始化

---
+++ src/scene/home.js
@@ -0,0 +1,23 @@
+/**
+ * 首页
+ */
+const ASYNC = require("async");
+const THREE = require("three");
+const TWEEN = require("@tweenjs/tween.js");
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * 功能：
+ * 创建地图：createMall
+ *
+ */
+
+export default class World {
+  constructor() {}
+  init() {
+    console.log('初始化world')
+  }
+}
+var world = new World()
+world.init()
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const pluginsConfig = require("./webpack.plugins.js");
+module.exports = {
+  entry: {
+    home: './src/scene/home.js',
+  },
+  mode: "development",
+  plugins: pluginsConfig,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties", { "loose": true }],
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: '8031',
+    disableHostCheck: true, // 取消host检查
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'map')
+  }
+};
